d114d6ba305a43bdf9f728b396829b953df7ff44
Marius Chirila <marius.chirila.91@gmail.com>
2021-11-18 13:00:17 +0100

    Create a basic bucket in GCP

---
+++ CodeReusable/gcloudbucket.py
@@ -0,0 +1,20 @@
+import sys
+from google.cloud import storage
+
+try: 
+    def main():
+        create_bucket(bucket_name)
+
+except Exception as e:
+    print(e)
+
+def create_bucket(bucket_name):
+    bucket_client = storage.Client()
+    bucket = bucket_client.bucket(bucket_name)
+    new_bucket = bucket_client.create_bucket(bucket)
+    print(new_bucket)
+
+bucket_name = sys.argv[1]
+
+if __name__ == '__main__':
+    main()
